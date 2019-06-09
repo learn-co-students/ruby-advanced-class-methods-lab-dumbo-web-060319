2f1dd5928b1c53d1686ca7235b9e0466d48795cc
Alexander Schelchere <ascellie@gmail.com>
2019-06-09 14:34:51 -0400

    Create and destroy!

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,33 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize
+    @@all << self
+  end
+
   def self.all
     @@all
   end
 
-  def save
-    self.class.all << self
+  def self.create
+    song = Song.new
   end
+
+  def self.new_by_name(name)
+
+  end
+
+  def self.create_by_name(name)
+
+  end
+
+  def self.find_or_create_by_name(name)
+    
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
 
 end
