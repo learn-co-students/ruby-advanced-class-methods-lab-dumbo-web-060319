92fa22779f256fe4dd7e3d9448f0639e00dd00eb
Alexander Schelchere <ascellie@gmail.com>
2019-06-09 14:52:42 -0400

    Helper function find all names added & find by name - complete

--- lib/song.rb
+++ lib/song.rb
@@ -25,19 +25,32 @@ class Song
   def self.new_by_name(name)
     song = Song.new
     song.name = name
+
     song
   end
 
   def self.create_by_name(name)
     song = new_by_name(name)
     @@all << song
-    
+
     song
   end
 
-  def self.find_or_create_by_name(name)
-    
+  def self.find_all_names
+    @@all.map { |song| song.name }
   end
+
+  def self.find_by_name(name)
+    @@all.find { |song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if Song.find_all_names.include? name
+        Song.find_by_name(name)
+    else
+        Song.create_by_name(name)
+    end
+  end 
 
   def self.alphabetical
     @@all.map{ |song| song.sort }
